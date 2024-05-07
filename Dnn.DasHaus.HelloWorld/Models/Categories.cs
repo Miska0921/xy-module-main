433aa070958acfcc4a6ed16dacf15d8b84c2f363
Miska0921 <vancskomiska@gmail.com>
2024-05-07 15:34:35 +0200

    Scaffold DB
    DB connect scaffoldolás

---
+++ Dnn.DasHaus.HelloWorld/Models/Categories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DasHaus.Dnn.Dnn.DasHaus.HelloWorld.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int Id { get; set; }
+        public string HotCakesId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
---
+++ Dnn.DasHaus.HelloWorld/Models/Products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetNuke.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DasHaus.Dnn.Dnn.DasHaus.HelloWorld.Models
+{
+    public partial class Products
+    {
+        public Products()
+        {
+            Reviews = new HashSet<Reviews>();
+        }
+
+        public int Id { get; set; }
+        public string HotCakesId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+
+        public virtual Categories Category { get; set; }
+        public virtual ICollection<Reviews> Reviews { get; set; }
+    }
+}
